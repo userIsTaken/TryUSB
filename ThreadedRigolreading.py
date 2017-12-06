a631abeaffbfe3a4dbda83f36d2afb3488455075
userIsTaken <internetui.007@gmail.com>
2017-12-06 17:02:06 +0200

    Threading

---
+++ ThreadedRigolreading.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtCore, QtWidgets, QtGui
+
+import sys, os
+from USBTMC_Devices import *
+from GetInfoAboutDevices import *
+from ConfigParser import *
+import vxi11
+import pyqtgraph as pG
+
