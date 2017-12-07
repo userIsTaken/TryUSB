a177aab070138715ddf2e0ec8453e42f43ce39a6
userIsTaken <internetui.007@gmail.com>
2017-12-07 14:07:42 +0200

    Separate classes for Siglent and Tektronix

--- Generators/SiglentGenerator.py
+++ Generators/SiglentGenerator.py
@@ -15,8 +15,12 @@ class SiglentGenerator_TCP():
                 self.Instrument = vxi11.Instrument(gen_path)
                 self.CH1 = "C1"
                 self.CH2 = "C2"
+                # channel 1 - CH1, channel 2 - CH2
                 pass
         
         def GetIDN(self):
                 name = self.Instrument.ask("*IDN?")
                 return name
+        
+        def EnableOutput(self, channel:str, out:bool):
+                pass
