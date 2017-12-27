e913a5e5b23bb25efdaaae80bdcda48c8d75d0f4
userIsTaken <internetui.007@gmail.com>
2017-12-27 17:00:26 +0200

    GUI Threading

--- MainExperimentLoopThread.py
+++ MainExperimentLoopThread.py
@@ -1,3 +1,21 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 import os, sys
+
+
+class LoopWorker(QThread):
+        
+        results = pyqtSignal(list, list, str)
+        errors = pyqtSignal(int, str)
+        final = pyqtSignal(int)
+        
+        def __init__(self, function, *args, **kwargs):
+                super(LoopWorker, self).__init__()
+                self.fn = function,
+                self.args = args
+                self.kwargs = kwargs
+                pass
+        
+        @pyqtSlot()
+        def run(self):
+                pass
