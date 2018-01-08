9c8d0c12c5db75c83d5bb01c1a2c3fe6d062c335
userIsTaken <internetui.007@gmail.com>
2018-01-08 21:33:00 +0200

    GUI initial threading

--- Units/UnitCheck.py
+++ Units/UnitCheck.py
@@ -1,10 +1,26 @@
 import math
 def getNumberSIprefix(value:float):
+        flt = None
+        unit = None
         log = math.floor(math.log10(value))
         if log < 0:
                 # value of log shows that we need multiply(!) by ten log times to get integer value
+                if log>-3:
+                        pass
+                if log<=-3  and log>-6:
+                        pass
+                if log<=-6:
+                        pass
                 pass
         elif log > 0:
+                # value of log shows that we need divide(!) by ten log times to get integer value
+                if log <3:
+                        pass
+                if log >= 3 and log < 6:
+                        pass
+                if log >= 6:
+                        pass
+                pass
                 pass
         else:
                 pass
