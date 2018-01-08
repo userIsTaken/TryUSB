6aeb0f3f06ff4a8f79a2a184bac31373e4dfb04b
userIsTaken <internetui.007@gmail.com>
2018-01-08 21:44:30 +0200

    GUI initial threading

--- Units/UnitCheck.py
+++ Units/UnitCheck.py
@@ -6,19 +6,30 @@ def getNumberSIprefix(value:float):
         if log < 0:
                 # value of log shows that we need multiply(!) by ten log times to get integer value
                 if log>-3:
-                        pass
+                        flt = value
+                        unit = ""
                 if log<=-3  and log>-6:
+                        flt = value * 1e3
+                        unit = "m"
                         pass
                 if log<=-6:
+                        flt = value * 1e6
+                        unit = "μ"
                         pass
                 pass
         elif log > 0:
                 # value of log shows that we need divide(!) by ten log times to get integer value
                 if log <3:
+                        flt = value
+                        unit = ""
                         pass
                 if log >= 3 and log < 6:
+                        flt = value / 1e3
+                        unit = "k"
                         pass
                 if log >= 6:
+                        flt = value / 1e6
+                        unit = "M"
                         pass
                 pass
                 pass
