17e5cdddc1cf579d6d258cf3e18751ac96bccf4d
userIsTaken <internetui.007@gmail.com>
2018-02-14 17:06:52 +0200

    Some fixes

--- Units/LimitsCheck.py
+++ Units/LimitsCheck.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy as sp
 
 def check_y_scale(*args, **kwargs):
+        need_scale = False
         y_array=args[0]
         length_of_y = len(y_array)
         np_y_array = np.array(y_array)
@@ -12,8 +13,6 @@ def check_y_scale(*args, **kwargs):
         array_of_greater_y = np_y_array[np_y_array>=minimum_limit_of_y]
         if len(array_of_greater_y) > 0.05*length_of_y:
                 print("out of limits and saturated")
-                pass
-        if max_y > "":
-                print("???")
-                pass
+                need_scale=True
+        return need_scale
         pass
