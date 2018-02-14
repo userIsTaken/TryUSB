0067e80a48b1696a54480d7cad882f6124bcf463
userIsTaken <internetui.007@gmail.com>
2018-02-14 16:35:39 +0200

    Some fixes

--- Units/LimitsCheck.py
+++ Units/LimitsCheck.py
@@ -6,5 +6,13 @@ import scipy as sp
 def check_y_scale(*args, **kwargs):
         arg_list=args
         kwarg_list=kwargs
-        
+        y_array=args[0]
+        length_of_y = len(y_array)
+        np_y_array = np.array(y_array)
+        max_y = np_y_array.max()
+        minimum_limit_of_y = 0.9*max_y
+        array_of_greater_y = np_y_array[np_y_array>=minimum_limit_of_y]
+        if len(array_of_greater_y) > 0.05*length_of_y:
+                print("out of limits")
+                pass
         pass
