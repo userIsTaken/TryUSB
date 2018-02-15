42300039a9dbf851cbba21c482c865d998fb5363
userIsTaken <internetui.007@gmail.com>
2018-02-15 15:45:00 +0200

    ale gerai

--- Units/LimitsCheck.py
+++ Units/LimitsCheck.py
@@ -9,10 +9,15 @@ def check_y_scale(*args, **kwargs):
         length_of_y = len(y_array)
         np_y_array = np.array(y_array)
         max_y = np_y_array.max()
-        minimum_limit_of_y = 0.9*max_y
+        minimum_limit_of_y = 0.985*max_y
         array_of_greater_y = np_y_array[np_y_array>=minimum_limit_of_y]
         if len(array_of_greater_y) > 0.05*length_of_y:
                 print("out of limits and saturated")
-                need_scale=True
-        return need_scale
+                print(array_of_greater_y)
+                print(max_y)
+                need_scale = True
+        else:
+                print("False")
+                print(max_y, "max y is masyvo")
+        return need_scale, max_y
         pass
