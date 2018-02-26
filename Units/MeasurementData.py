f72f89290d26f657d8bca831270a786885c14773
userIsTaken <internetui.007@gmail.com>
2018-02-26 14:15:07 +0200

    Ability to save data from oscilloscope into CSV data

--- Units/MeasurementData.py
+++ Units/MeasurementData.py
@@ -2,7 +2,7 @@
 #-*- codin: utf-8 -*-
 import os, sys, csv
 import numpy as np
-
+from Units.Functions import *
 
 class MeasurementData(csv):
         def __init__(self, filename):
@@ -11,8 +11,13 @@ class MeasurementData(csv):
                 self.fileName = filename # filename
                 pass
         
-        def set_data_arrays(self, *args):
+        def set_data_arrays(self,R,S, *args):
                 try:
+                        time_array = args[0]
+                        time_unit = args[1]
+                        channel_one = args[2]
+                        channel_two =args[3]
+                        channel_as_current_density = getCurrentDensity(channel_two, R, S)
                         pass
                 except Exception as ex:
                         print("FCUK")
